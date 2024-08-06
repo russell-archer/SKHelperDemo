1d21d4bbd740208060167e571a22366ff2af16a2
Russell Archer <russell.archer@mac.com>
2024-08-06 18:46:03 +0100

    Initial Commit

---
+++ SKHelperDemo/SKHelperDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKHelperDemoApp.swift
+//  SKHelperDemo
+//
+//  Created by Russell Archer on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SKHelperDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
